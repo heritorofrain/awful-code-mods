f47cc88afa31a8999404e2c1dcb47f17b025f7fb
Heritorofarc <88900822+Heritorofarc@users.noreply.github.com>
2025-12-11 07:05:52 -0500

    Add files via upload

---
+++ src/baseconvo.rs
@@ -0,0 +1,29 @@
+#![feature(lazy_cell, ptr_sub_ptr)]
+use unity::{prelude::*};
+use unity::system::List;
+use engage::gamedata::unit::Unit;
+use engage::gamedata::Hub::*;
+
+#[unity::class("App", "HubLocatorGroup.CreateCharacter")]
+pub struct CreateCharacter {
+    pub active_group_root: u64,
+    pub unit_list: u64,
+    pub access_list: u64,
+    pub active: bool,
+    pub system_active: bool,
+    pub event_active: bool,
+    pub inactive_objects: u64,
+    pub loading_chara_count: i32,
+    pub hash_table: u64,
+}
+
+#[unity::class("App", "HubLocatorGroup.CreateCharacter")]
+//#[skyline::hook(offset=0x028ae890)]
+pub fn hubunit_makespawn(this: &mut CreateCharacter, pid: &Il2CppString, locator: &mut HubLocatorGroup, access: &HubAccessData, callback: u64, method_info: OptionalMethod) {
+    call_original!(this, pid, locator, access, callback, method_info);
+    println!("plugin did not crash, good job on the bare minimum lmao")
+}
+#[skyline::main(name = "hooks")]
+pub fn main() {
+    skyline::install_hook!(hubunit_makespawn);
+}
